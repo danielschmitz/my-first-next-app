464eae5c375966760b79a929693c01a60f73ebf4
Daniel Schmitz <danieljfa@gmail.com>
2021-03-22 17:05:52 -0300

    getStaticPaths

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,13 +1,30 @@
 
-export async function getServerSideProps(context) {
-    const id = context.query.id
+export async function getStaticPaths(context) {
+    const posts = [1, 2, 3, 4, 5];
+    const paths = posts.map((id) => ({
+      params: { id: `${id}` },
+    }));
+    return { paths, fallback: false };
+  }
+
+
+export async function getStaticProps ({params}) {
+    const id = params.id
+
+    // Dummy get Product from table
+    const product = {
+        id,
+        name: `Product ${id}`
+    }
+    console.log(`dummy: get product from table with id ${id}`)
+
     return {
         props: {
-            id: id
+            product
         }
     }
 }
 
 export default function Product(props) {
-    return <p>Show Product with id {props.id} </p>
+    return <p>Show Product with id {props.product.id} and name '{props.product.name}' </p>
 }
