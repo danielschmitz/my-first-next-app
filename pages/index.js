75fcacd912db6c5567c7688279878d46773d4f13
Daniel Schmitz <danieljfa@gmail.com>
2021-03-22 08:59:08 -0300

    counter example

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+
+const Counter = (props) => {
+  const [count,setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  function addOne() {
+    setCount(count+1)
+    console.log(count);
+  }
+
+  return (
+    <div>
+      <b>Count: {count}</b>
+      <br />
+      <br />
+      <button onClick={addOne}>Add One</button>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <div>
-      <Link href="/foo">
-        <a>Foo Page</a>
-      </Link>
+      <Counter />
     </div>
   );
 }
